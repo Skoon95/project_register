435a54c94ef9f8de9cd0b417f511c73efc27ae52
YSH <tjrgns21@naver.com>
2023-06-08 15:48:50 +0900

    project register

--- src/main/resources/static/resources/scripts/register.js
+++ src/main/resources/static/resources/scripts/register.js
@@ -1,9 +1,15 @@
 const registerForm = document.getElementById('registerForm');
 
+registerForm
+
+
 registerForm.onsubmit = e => {
     e.preventDefault();
-    if (registerForm.classList.contains('step-1')) {
+    if (registerForm.classList.contains('step-1')){
+
+
         registerForm.classList.remove('step-1');
         registerForm.classList.add('step-2');
     }
-};
+}
+
