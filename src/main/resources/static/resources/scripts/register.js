ef25ccc6e7dccc1654b2e2154561a5b91db0f52a
ysh <tjrgns21@naver.com>
2023-06-11 04:56:36 +0900

    06 10

--- src/main/resources/static/resources/scripts/register.js
+++ src/main/resources/static/resources/scripts/register.js
@@ -1,15 +1,43 @@
 const registerForm = document.getElementById('registerForm');
+const dialogCover = document.getElementById('dialogCover');
+const addressLayer = document.getElementById('addressLayer');
 
-registerForm
+const showAddressLayer = () => {
+    new daum.Postcode({
+        oncomplete: (data) => {
+            registerForm['addressPostal'].value = data.zonecode;
+            registerForm['addressPrimary'].value = data.address;
+            registerForm['addressSecondary'].value = '';
+            registerForm['addressSecondary'].focus();
+            hideDialogCover();
+            hideAddressLayer();
+        }
+    }).embed(addressLayer);
+    addressLayer.classList.add('visible');
+};
 
+const hideDialogCover = () => {
+    dialogCover.style.opacity = '0';
+    dialogCover.style.pointerEvents = 'none';
+};
+
+const hideAddressLayer = () => {
+    addressLayer.style.opacity = '0';
+    addressLayer.style.pointerEvents = 'none';
+    addressLayer.style.transform = 'scale(90%) translate(-50%, -50%)';
+};
 
 registerForm.onsubmit = e => {
     e.preventDefault();
     if (registerForm.classList.contains('step-1')){
-
-
         registerForm.classList.remove('step-1');
         registerForm.classList.add('step-2');
     }
-}
+};
+
+registerForm['addressFind'].onclick = () => {
+    dialogCover.style.opacity = '1';
+    dialogCover.style.pointerEvents = 'all';
+    showAddressLayer();
+};
 
